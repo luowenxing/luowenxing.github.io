27d3651e24857b34e9d800cbb6343c8e14861a81
Luo <511352272@qq.com>
2017-07-27 17:36:15 +0800

    fix indent issue

---
+++ GoBang/js/Piece.js
@@ -0,0 +1,13 @@
+(function (exports) {
+
+    function Piece(x, y, isBlack) {
+        this.isBlack = isBlack
+        this.x = x
+        this.y = y
+        // 每一个棋子包含赢棋提示的队列
+        this.winIndications = []
+    }
+
+    exports.Piece = Piece
+})(window)
+
